4f7a079db977f116b3700e5eb7b43cc754aa7a6c
Karol Legut <karol.legut121@gmail.com>
2025-03-10 21:35:20 +0100

    feat: enhance Terraform configuration for Azure with container registry and secure proxy setup

--- main.tf
+++ main.tf
@@ -1,117 +1,172 @@
+locals {
+  sanitized_name = lower(replace(var.name, "/[^a-z0-9]/", "-"))
+  key_vault_name = "${local.sanitized_name}-vault"
+  key_name       = "${local.sanitized_name}-key"
+
+  container_sku = var.enable_key_vault ? "Confidential" : "Standard"
+  key_type      = var.enable_key_vault ? "azure-skr" : "ephemeral"
+
+  vnet_resource_group_name = var.vnet_resource_group != "" ? var.vnet_resource_group : azurerm_resource_group.main.name
+
+  subnet_id = var.create_new_vnet ? (
+    azurerm_subnet.main[0].id
+    ) : (
+    "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.vnet_resource_group_name}/providers/Microsoft.Network/virtualNetworks/${var.vnet_name}/subnets/${var.subnet_name}"
+  )
+
+  attestation_policy = {
+    version = "1.0.0"
+    anyOf = [
+      {
+        authority = "https://sharedweu.weu.attest.azure.net"
+        allOf = concat(
+          [
+            {
+              claim  = "x-ms-attestation-type"
+              equals = "sevsnpvm"
+            },
+            {
+              claim  = "x-ms-compliance-status"
+              equals = "azure-compliant-uvm"
+            }
+          ],
+        )
+      }
+    ]
+  }
+}
+
 resource "azurerm_resource_group" "main" {
-  name     = "${var.name}-rg"
+  name     = "${local.sanitized_name}-rg"
   location = var.location
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.name}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
+# Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "${replace(lower(var.name), "-", "")}acr"
   resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "main" {
-  name                 = "${var.name}-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "main" {
-  count               = var.public_ip_type != "NONE" ? 1 : 0
-  name                = "${var.name}-pip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = var.public_ip_type
+  location            = var.location
   sku                 = "Standard"
+  admin_enabled       = true
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.name}-nic"
-  location            = azurerm_resource_group.main.location
+# Import images to ACR
+resource "null_resource" "import_images" {
+  triggers = {
+    acr_id = azurerm_container_registry.acr.id
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+    az acr import --name ${azurerm_container_registry.acr.name} \
+        --source "fr0ntierxpublic.azurecr.io/polaris-proxy":${var.polaris_proxy_image_version} \
+      --image polaris-proxy:${var.polaris_proxy_image_version} \
+      --force
+    
+    az acr import --name ${azurerm_container_registry.acr.name} \
+      --source mcr.microsoft.com/aci/skr:2.7 \
+      --image skr-sidecar:2.7 \
+      --force
+  EOT
+  }
+}
+
+# Container Group
+resource "azurerm_container_group" "main" {
+  name                = "${local.sanitized_name}-aci"
   resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.main.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = var.public_ip_type != "NONE" ? azurerm_public_ip.main[0].id : null
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "main" {
-  name                  = "${var.name}-vm"
-  resource_group_name   = azurerm_resource_group.main.name
-  location              = azurerm_resource_group.main.location
-  size                  = var.vm_size
-  admin_username        = "adminuser"
-  network_interface_ids = [azurerm_network_interface.main.id]
-
-  secure_boot_enabled = true
-  vtpm_enabled        = true
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = var.ssh_public_key_path
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = var.boot_disk_type
-    disk_size_gb         = var.boot_disk_size
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-confidential-vm-jammy"
-    sku       = "22_04-lts-cvm"
-    version   = "latest"
-  }
-
-  custom_data = base64encode(templatefile("${path.module}/cloud-init.yml", {
-    enable_kms                  = var.enable_kms
-    key_vault_name              = var.enable_kms ? azurerm_key_vault.main[0].name : ""
-    key_name                    = var.enable_kms ? azurerm_key_vault_key.main[0].name : ""
-    managed_identity_client_id  = azurerm_user_assigned_identity.main.client_id
-    polaris_proxy_image         = var.polaris_proxy_image
-    polaris_proxy_image_version = var.polaris_proxy_image_version
-    workload_image              = var.workload_image
-    workload_port               = var.workload_port
-    polaris_proxy_port          = var.polaris_proxy_port
-    workload_env_vars           = var.workload_env_vars  # Pass directly, it's already a map
-    workload_entrypoint         = var.workload_entrypoint
-    workload_arguments          = var.workload_arguments
-    tenant_id                   = data.azurerm_client_config.current.tenant_id
-    attestation_provider_name   = var.enable_kms ? replace(lower("${var.name}attestation"), "-", "") : ""
-    attestation_provider_uri    = var.enable_kms ? azurerm_attestation_provider.main[0].attestation_uri : ""
-    polaris_proxy_enable_input_encryption  = var.polaris_proxy_enable_input_encryption
-    polaris_proxy_enable_output_encryption = var.polaris_proxy_enable_output_encryption
-    polaris_proxy_enable_cors             = var.polaris_proxy_enable_cors
-    polaris_proxy_enable_logging          = var.polaris_proxy_enable_logging
-    key_type                    = "azure-federated"
-    location                    = var.location
-  }))
-
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.main.id]
-  }
-}
-
-resource "azurerm_network_security_group" "main" {
-  name                = "${var.name}-nsg"
   location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  os_type             = "Linux"
+  ip_address_type     = var.networking_type == "Private" ? "Private" : "Public"
+  dns_name_label      = var.dns_name_label
+  subnet_ids          = var.networking_type == "Private" ? [local.subnet_id] : null
+  sku                 = local.container_sku
+  dynamic "identity" {
+    for_each = var.enable_key_vault ? [1] : []
+    content {
+      type = "SystemAssigned"
+    }
+  }
 
-  security_rule {
-    name                       = "allow-polaris-proxy"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = var.polaris_proxy_port
-    source_address_prefixes    = var.polaris_proxy_source_ranges
-    destination_address_prefix = "*"
+  depends_on = [null_resource.import_images]
+
+  # SKR Sidecar - tylko gdy enable_key_vault=true
+  dynamic "container" {
+    for_each = var.enable_key_vault ? [1] : []
+    content {
+      name   = "skr-sidecar-container"
+      image  = "${azurerm_container_registry.acr.login_server}/skr-sidecar:2.7"
+      cpu    = "1"
+      memory = "1"
+
+      commands = ["/skr.sh"]
+
+      environment_variables = {
+        "POLARIS_CONTAINER_AZURE_SKR_MAA_ENDPOINT" = "sharedweu.weu.attest.azure.net"
+        "POLARIS_CONTAINER_AZURE_SKR_AKV_ENDPOINT" = "${local.key_vault_name}.vault.azure.net"
+        "POLARIS_CONTAINER_AZURE_SKR_KID"          = local.key_name
+      }
+
+      ports {
+        port     = 8080
+        protocol = "TCP"
+      }
+    }
+  }
+
+  # Polaris Proxy
+  container {
+    name   = "polaris-secure-proxy"
+    image  = "${azurerm_container_registry.acr.login_server}/polaris-proxy:${var.polaris_proxy_image_version}"
+    cpu    = "1"
+    memory = "1"
+
+    environment_variables = {
+      POLARIS_CONTAINER_KEY_TYPE                 = local.key_type
+      POLARIS_CONTAINER_WORKLOAD_BASE_URL        = "http://localhost:${var.workload_port}"
+      POLARIS_CONTAINER_ENABLE_INPUT_ENCRYPTION  = var.polaris_proxy_enable_input_encryption
+      POLARIS_CONTAINER_ENABLE_OUTPUT_ENCRYPTION = var.polaris_proxy_enable_output_encryption
+      POLARIS_CONTAINER_ENABLE_CORS              = var.polaris_proxy_enable_cors
+      POLARIS_CONTAINER_ENABLE_LOGGING           = var.polaris_proxy_enable_logging
+      POLARIS_CONTAINER_AZURE_SKR_MAA_ENDPOINT   = "sharedweu.weu.attest.azure.net"
+      POLARIS_CONTAINER_AZURE_SKR_AKV_ENDPOINT   = "${local.key_vault_name}.vault.azure.net"
+      POLARIS_CONTAINER_AZURE_SKR_KID            = local.key_name
+    }
+
+    ports {
+      port     = var.polaris_proxy_port
+      protocol = "TCP"
+    }
+  }
+
+  # Client workload
+  container {
+    name   = "workload"
+    image  = var.registry_login_server != "" ? "${var.registry_login_server}/${var.workload_image}" : var.workload_image
+    cpu    = var.container_cpu
+    memory = var.container_memory
+
+    environment_variables = var.workload_env_vars
+    commands              = var.workload_arguments
+
+    ports {
+      port     = var.workload_port
+      protocol = "TCP"
+    }
+  }
+
+  image_registry_credential {
+    server   = azurerm_container_registry.acr.login_server
+    username = azurerm_container_registry.acr.admin_username
+    password = azurerm_container_registry.acr.admin_password
+  }
+
+  dynamic "image_registry_credential" {
+    for_each = var.registry_login_server != "" ? [1] : []
+    content {
+      server   = var.registry_login_server
+      username = var.registry_username
+      password = var.registry_password
+    }
   }
 }
